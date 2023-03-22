869db706d9b6b1a8a7906aca7a9a0068e69ffbb1
andreycmartins <andreybr45@gmail.com>
2023-03-22 15:50:19 -0300

    Done

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,8 +1,67 @@
-﻿Console.Clear();
+﻿using System.Runtime.Serialization;
 
-Console.WriteLine("primeiro numero");
-int v1 = int.Parse(Console.ReadLine());
-Console.WriteLine("segundo numero");
-int v2 = int.Parse(Console.ReadLine());
-int resultado = v1 + v2;
-Console.WriteLine("A soma é: " + resultado);
+Console.Clear();
+Console.WriteLine("Qual tipo de conta deseja fazer?");
+Console.WriteLine("1 - Soma");
+Console.WriteLine("2 - Subtração");
+Console.WriteLine("3 - Multiplicação");
+Console.WriteLine("4 - Divisão");
+int tipoDeConta = int.Parse(Console.ReadLine());
+Console.WriteLine(tipoDeConta);
+
+switch (tipoDeConta)
+{
+    case 1: Soma(); break;
+    case 2: Subtracao(); break;
+    case 3: Multiplicacao(); break;
+    case 4: Divisao(); break;
+}
+
+static void Soma()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o primeiro valor: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+
+    Console.WriteLine("Digite o segundo valor: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("O resultado da soma é: " + (v1+v2));
+}
+static void Subtracao()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o primeiro valor: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+
+    Console.WriteLine("Digite o segundo valor: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("O resultado da subtração é: " + (v1 - v2));
+}
+static void Multiplicacao()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o primeiro valor: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+
+    Console.WriteLine("Digite o segundo valor: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("O resultado da multiplicação é: " + (v1 * v2));
+}
+static void Divisao()
+{
+    Console.Clear();
+    Console.WriteLine("Digite o primeiro valor: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+
+    Console.WriteLine("Digite o segundo valor: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("O resultado da Divisão é: " + (v1 / v2));
+}
